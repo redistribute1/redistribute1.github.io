caba5a67ae9a82da605fb0bf08bf5f7211cf6b95
Naevin <naevin.anukornchaikul@gmail.com>
2019-09-14 17:45:16 -0400

    updated details page

--- event-details.js
+++ event-details.js
@@ -49,15 +49,14 @@ function render_eventdetails_page() {
                 <span class="organ_name"><strong>Organization Name:</strong> ${doc.data().organization}</span><br>
                 <span class="type"><strong>Charity Type:</strong> ${doc.data().type}</span><br><br>
                 <span class="description"><strong>Descripton:</strong> ${doc.data().description}</span><br>
-                <span class="supplieslist"><strong>Supplies:</strong></span><br>
+                
                 <span class="volunteers"><strong>Volunteer List:</strong></span>
                 <table>
                     <tr>
                         <th>Name</th>
                         <th>Phone</th>
                         <th>Email</th>
-                    </tr>
-                    
+                    </tr> 
             `
             volunteerstring = doc.data().volunteers;
             varray = volunteerstring.split(";");
@@ -72,8 +71,32 @@ function render_eventdetails_page() {
                 </tr>
                 `
             }
-            //TODO CHANGE THIS FOR FINAL PRODUCT 
-            html = html + `</table>`;  
+            html = html + `
+            </table><br><br>
+            <span class="volunteers"><strong>Supplies:</strong></span>
+            <table>
+                    <tr>
+                        <th>Item Description</th>
+                        <th>Amount</th>
+                    </tr> `; 
+
+                    
+            rstring = doc.data().resources;
+            rarray = rstring.split(";");
+            var j = 0;
+            for(j = 0; j < rarray.length; j++) {
+                item_array = rarray[j].split('-');
+                console.log(item_array);
+                html = html + `
+                <tr>
+                    <td>${item_array[0]}</td>
+                    <td>${item_array[1]}</td>
+                </tr>
+                `
+            }
+            html = html + `
+            </table><br><br>`
+
             event_detailsID.innerHTML = html;
             
         }
