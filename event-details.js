140b4b080bc4f11237e7da8f404482654dd0a9ee
Naevin <naevin.anukornchaikul@gmail.com>
2019-09-14 21:30:40 -0400

    finished the details page for realtime updates

--- event-details.js
+++ event-details.js
@@ -34,6 +34,9 @@ function setupUI(user) {
     }
 }
 
+const volunteerform = document.querySelector('#volunteer_form');
+const rform = document.querySelector('#resource_form');
+
 //render the event details page
 function render_eventdetails_page() {
     var getValue = localStorage.getItem('id');
@@ -81,7 +84,7 @@ function render_eventdetails_page() {
                         <th>Amount</th>
                     </tr> `; 
 
-                    
+            
             rstring = doc.data().resources;
             rarray = rstring.split(";");
             var j = 1;
@@ -106,7 +109,76 @@ function render_eventdetails_page() {
     
 }
 
-const volunteerform = document.querySelector('#volunteer_form');
+db.collection("event-list").doc(localStorage.getItem('id'))
+    .onSnapshot(function(doc) {
+        console.log("test");
+        var event_detailsID = document.getElementById("event_detail_box");
+            
+            html = `
+                <span class="title"><strong>${doc.data().title}</strong></span><br>
+                <span class="date"><strong>Date:</strong> ${doc.data().date}</span><br>
+                <span class="address"><strong>Address:</strong> ${doc.data().address}</span><br>
+                <span class="organ_name"><strong>Organization Name:</strong> ${doc.data().organization}</span><br>
+                <span class="type"><strong>Charity Type:</strong> ${doc.data().type}</span><br><br>
+                <span class="description"><strong>Descripton:</strong> ${doc.data().description}</span><br>
+                
+                <span class="volunteers"><strong>Volunteer List:</strong></span>
+                <table>
+                    <tr>
+                        <th>Name</th>
+                        <th>Phone</th>
+                        <th>Email</th>
+                    </tr> 
+            `
+            volunteerstring = doc.data().volunteers;
+            varray = volunteerstring.split(";");
+            var i = 3;
+            
+            for(i = 3; i < varray.length-1; i+=3) {
+                
+                html = html + `
+                <tr>
+                    <td>${varray[i]}</td>
+                    <td>${varray[i+1]}</td>
+                    <td>${varray[i+2]}</td>
+                </tr>
+                `
+            }
+            html = html + `
+            </table><br><br>
+            <span class="volunteers"><strong>Supplies:</strong></span>
+            <table>
+                    <tr>
+                        <th>Item Description</th>
+                        <th>Amount</th>
+                    </tr> `; 
+
+            
+            rstring = doc.data().resources;
+            rarray = rstring.split(";");
+            var j = 1;
+            for(j = 1; j < rarray.length-1; j++) {
+                item_array = rarray[j].split('-');
+                console.log(item_array);
+                html = html + `
+                <tr>
+                    <td>${item_array[0]}</td>
+                    <td>${item_array[1]}</td>
+                </tr>
+                `
+            }
+            html = html + `
+            </table><br><br>`
+            event_detailsID.innerHTML = html;
+            volunteerform.name.value = '';
+            volunteerform.phone.value = '';
+            volunteerform.email.value = '';
+            rform.name.value = '';
+            rform.quantity.value = '';
+        
+});
+
+
 volunteerform.addEventListener('submit', (e) => {
     e.preventDefault();
 
@@ -126,7 +198,7 @@ volunteerform.addEventListener('submit', (e) => {
     });
 });
 
-const rform = document.querySelector('#resource_form');
+
 rform.addEventListener('submit', (e) => {
     e.preventDefault();
 
