34f497d9cd32e284905f3c2031f61b8198f4bdb7
Naevin <naevin.anukornchaikul@gmail.com>
2019-09-14 22:53:47 -0400

    test

--- myevents.js
+++ myevents.js
@@ -1,10 +1,44 @@
-const event_list = document.querySelector('#event-list'); //reference main page event list
-var googlemap = document.getElementById("map"); //reference google map being displayed on the landing page
+window.onload = function(){
+    $.get("/navBar.html", function(data){
+        $("#include").html(data);
+    })
+}
+
+auth.onAuthStateChanged(user => {
+    /* if the user is logged in allowed for account features
+     * otherwise simply display website as guest
+     */
+    if (user) {
+        console.log('Main Script: ' + user);
+        setupUI(user);
+    } else {
+        console.log('Main Script: no user logged in');
+        setupUI();
+    }
+});
+
+//setup up UI based on logged in out logged out
+function setupUI(user) {
+    const logged_out_links = document.querySelectorAll('.logged_out');
+    const logged_in_links = document.querySelectorAll('.logged_in');
+    if (user) {
+        //user is logged in 
+        logged_in_links.forEach(item => item.style.display = 'block');
+        logged_out_links.forEach(item => item.style.display = 'none');
+        
+    } else {
+        //user is logged out
+        logged_in_links.forEach(item => item.style.display = 'none');
+        logged_out_links.forEach(item => item.style.display = 'block');
+    }
+}
+
 
 function convert_list_element(doc) {
     html = `
     <li event-id=${doc.id} class="listelement">
         <button class="details" onclick="upload_event_item_info('${doc.id}')">View Details</button>
+        <button class="delete" onclick="delete_item_info('${doc.id}')">Delete Event</button>
         <span class="title"><strong>${doc.data().title}</strong></span><br>
         <span class="date"><strong>Date:</strong> ${doc.data().date}</span><br>
         <span class="address"><strong>Address:</strong> ${doc.data().address}</span><br>
@@ -23,10 +57,19 @@ function upload_event_item_info(docid) {
 
 }
 
+function delete_item_info(docid) {
+    db.collection("event-list").doc(docid).delete().then(function() {
+        console.log("Document successfully deleted!");
+    }).catch(function(error) {
+        console.error("Error removing document: ", error);
+    });
+}
+
 //real time listener to query the charity event elements.
+const event_list = document.querySelector('#myevent-list'); //reference main page event list
 db.collection('event-list').orderBy('date').onSnapshot(snapshot => {
     let changes = snapshot.docChanges();
-    var event_listID = document.getElementById("event-list");
+    var event_listID = document.getElementById("myevent-list");
     var finalhtml = "";
     
     //for each element in the query
@@ -37,7 +80,6 @@ db.collection('event-list').orderBy('date').onSnapshot(snapshot => {
             element = convert_list_element(change.doc);
             
         } else if (change.type == 'removed') {
-            console.log("index remove");
             let li = event_list.querySelector('[event-id=' + change.doc.id + ']');
             event_list.removeChild(li);
         }
@@ -45,3 +87,8 @@ db.collection('event-list').orderBy('date').onSnapshot(snapshot => {
     });
     event_listID.innerHTML = finalhtml;
 })
+
+//logout function
+function logout() {
+    auth.signOut();
+}
